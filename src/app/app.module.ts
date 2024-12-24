7e170a35c15c80b5fa70cda1e2e422f68a45fbac
Asma CHKIRIDA <asmachkirida02@gmail.com>
2024-12-24 16:08:48 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  // Add FormsModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+import { NgxEchartsModule } from 'ngx-echarts';
+import { MainAppComponent } from './main-app/main-app.component';
+import { HttpClientModule } from '@angular/common/http'; // Import this
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    MainAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,           // Add this
+    ReactiveFormsModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')  // Proper configuration for echarts
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
